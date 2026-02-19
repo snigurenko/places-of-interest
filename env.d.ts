b5c4667b683e5d687d83b37c321be01f6833cfa6
Dymitr Snigurenko <d.snigurenko@smartcat.ai>
2026-02-19 15:08:18 +0100

    delete defaults components and add mapbox and rapidapi keys

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
-/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_MAPBOX_TOKEN: string
+  readonly VITE_OPENTRIPMAP_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+// TODO: add after create accoutns
+VITE_MAPBOX_TOKEN=your_token_here
+VITE_OPENTRIPMAP_KEY=your_key_here
