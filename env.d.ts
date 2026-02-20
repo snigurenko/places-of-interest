f0b760db7aaf17f124943a4999b6b679b607140b
Dymitr Snigurenko <61964812+snigurenko@users.noreply.github.com>
2026-02-20 13:22:55 +0100

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 interface ImportMetaEnv {
   readonly VITE_RAPIDAPI_KEY: string
   readonly VITE_MAPBOX_TOKEN: string
-  readonly BASE_URL: string  // ← добавь эту строку
+  readonly BASE_URL: string
 }
 
 interface ImportMeta {
